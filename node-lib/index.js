99f4f85f6904d9d4f3530c46caf1c01ca45bbc39
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2023-10-27 13:07:59 -0300

    Arrumando dependências

--- node-lib/index.js
+++ node-lib/index.js
@@ -1,1 +1,29 @@
-console.log('olá mundo');
+import fs from 'fs';
+import chalk from 'chalk';
+
+function trataErro(erro) {
+    //throw new Error(erro);
+    throw new Error(chalk.red(erro.code, "Arquivo não encontrado"));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile (caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+
+/*
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
+    });
+}
+*/
+//pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto');
